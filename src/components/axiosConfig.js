fdb4b0c2398b6b215ce3d93d9c6104810b7ef50b
yagowb <yagodev3@gmail.com>
2023-06-09 01:51:07 -0300

    Ajuste de URL em axiosConfig

--- src/components/axiosConfig.js
+++ src/components/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://tocaplay-webapp-backend-g2k4bo6cg-yagowb.vercel.app' 
+  baseURL: 'https://tocaplay-webapp-backend-i3pjltjk9-yagowb.vercel.app' 
   
   
 });
