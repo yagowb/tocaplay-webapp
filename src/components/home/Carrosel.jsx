93464f6c8b1c2e8e5e9507d7845bc82c73370a4b
yagowb <yagodev3@gmail.com>
2023-06-09 18:40:18 -0300

    Ajuste motion Carrossel

--- src/components/home/Carrosel.jsx
+++ src/components/home/Carrosel.jsx
@@ -1,47 +1,38 @@
-import {useState, useEffect, useRef} from 'react';
-import {motion} from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import instance from '../axiosConfig';
 
-
 function Carrosel() {
-
   const carrosel = useRef();
-  const [width, setWidth] = useState(0)
+  const [width, setWidth] = useState(0);
+  const [playlists, setPlaylists] = useState([]);
 
-  const [playlists, setPlaylists] = useState([])
-
-  useEffect(() =>{
+  useEffect(() => {
     instance.get('/playlists')
-      .then(res=>
-      (setPlaylists(res.data)
-      (setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth))
-      ))
-    
-  }, [])
-
-
+      .then(res => {
+        setPlaylists(res.data);
+        setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth);
+      });
+  }, []);
 
   return (
     <div className="carrosel-content">
-    
-      <motion.div ref={carrosel} className='carrosel' whileTap={{cursor: "grabbing"}}>
-          <motion.div 
-          className='inner'
+      <motion.div ref={carrosel} className="carrosel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div
+          className="inner"
           drag="x"
-          dragConstraints={{right:0, left: -width}}
-          >
-            {playlists.map(image => (
-              <motion.div className='item' key={image}>
-                  <img src ={image.capa} alt="texto alt"/>
-                <Link to={`/playlists/${image._id}`}>
-                  <button className='botao-playlist'>Acessar Playlist</button>
-                </Link>
-              </motion.div>
-              ))}
-              
-
-          </motion.div>
+          dragConstraints={{ right: 0, left: -width }}
+        >
+          {playlists.map(image => (
+            <motion.div className="item" key={image}>
+              <img src={image.capa} alt="texto alt" />
+              <Link to={`/playlists/${image._id}`}>
+                <button className="botao-playlist">Acessar Playlist</button>
+              </Link>
+            </motion.div>
+          ))}
+        </motion.div>
       </motion.div>
     </div>
   );
