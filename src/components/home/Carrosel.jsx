01a5708cd57ec827953a10458565dc047b685714
yagowb <yagodev3@gmail.com>
2023-06-09 22:53:58 -0300

    Ajuste tentativa carrossel

--- src/components/home/Carrosel.jsx
+++ src/components/home/Carrosel.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import instance from '../axiosConfig';
 
 function Carrosel() {
-  const carrosel = useRef();
+  const carouselRef = useRef();
   const [width, setWidth] = useState(0);
   const [playlists, setPlaylists] = useState([]);
 
@@ -12,23 +12,23 @@ function Carrosel() {
     instance.get('/playlists')
       .then(res => {
         setPlaylists(res.data);
-        setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth);
+        setWidth(carouselRef.current?.scrollWidth - carouselRef.current?.offsetWidth);
       });
   }, []);
 
   return (
-    <div className="carrosel-content">
-      <motion.div ref={carrosel} className="carrosel" whileTap={{ cursor: "grabbing" }}>
+    <div className="carousel-content">
+      <motion.div ref={carouselRef} className="carousel" whileTap={{ cursor: "grabbing" }}>
         <motion.div
           className="inner"
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
         >
-          {playlists.map(image => (
-            <motion.div className="item" key={image}>
-              <img src={image.capa} alt="texto alt" />
-              <Link to={`/playlist/${image._id}`}>
-                <button className="botao-playlist">Acessar Playlist</button>
+          {playlists.map(playlist => (
+            <motion.div className="item" key={playlist._id}>
+              <img src={playlist.capa} alt="Playlist cover" />
+              <Link to={`/playlist/${playlist._id}`}>
+                <button className="playlist-button">Acessar Playlist</button>
               </Link>
             </motion.div>
           ))}
